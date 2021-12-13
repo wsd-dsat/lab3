5ec239aab4937b3144949b43e663f780fe30efcf
wsd-dsat <78375117+wsd-dsat@users.noreply.github.com>
2021-12-13 22:34:02 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+//zad2
+//#include <stdio.h>
+//
+//int main() {
+//  
+//  FILE *fptr;
+//  
+//  char ch;
+//  fptr = fopen("/home/qwc/Документы/alg/lab3/ex1.txt", "a");
+//  printf("vvedit symvol: ");
+//  
+//  while( (ch = getchar()) != '\n' ) {
+//    putc(ch, fptr);
+//
+//  }
+//  
+//  fclose(fptr);
+//  
+//  fopen("/home/qwc/Документы/alg/lab3/ex1.txt", "r");
+//  while( (ch = getc(fptr)) != EOF ) {
+//    printf("%c", ch);
+//  }
+//  fclose(fptr);
+//  
+//  return 0;
+//}
+
